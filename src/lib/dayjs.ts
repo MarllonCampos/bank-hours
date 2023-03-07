682244fea817fbb50e8a5eeb066bf5e1cfadd12f
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-03-07 04:28:37 -0300

    feat: import locale data plugin

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -3,7 +3,9 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/pt-br'
 
 import duration from "dayjs/plugin/duration";
+import localeData from "dayjs/plugin/localeData";
 
 
 dayjs.extend(duration);
+dayjs.extend(localeData);
 dayjs.locale('pt-br')
