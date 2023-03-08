fc901c83aa5d563f241f87f964fe1eaf64ad0894
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-03-08 15:23:31 -0300

    feat: add firebase to project

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./lib/dayjs";
 import { AppRoutes } from "./routes";
-
+import "./services/firebase";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AppRoutes />
